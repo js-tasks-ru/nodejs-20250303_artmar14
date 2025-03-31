ec9657c1a220607309e6fd1b69154528486dc242
artm4r <artmar14@gmail.com>
2025-03-31 11:39:52 +0300

    solution for `05-db-01-crud-tasks`

--- 05-db/01-crud-tasks/tasks/tasks.controller.ts
+++ 05-db/01-crud-tasks/tasks/tasks.controller.ts
@@ -1,22 +1,35 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe, Query } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
+import { GetTasksDto } from "./dto/get-tasks.dto";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create() {}
+  create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto)
+  }
 
   @Get()
-  findAll() {}
+  findAll(@Query() query: GetTasksDto) {
+    return this.tasksService.findAll(query)
+  }
 
   @Get(":id")
-  findOne() {}
+  findOne(@Param("id", ParseIntPipe) id: number) {
+    return this.tasksService.findOne(id);
+  }
 
   @Patch(":id")
-  update() {}
+  update(@Param("id", ParseIntPipe) id: number, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.tasksService.update(id, updateTaskDto)
+  }
 
   @Delete(":id")
-  remove() {}
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.tasksService.remove(id);
+  }
 }
--- 05-db/01-crud-tasks/tasks/tasks.service.ts
+++ 05-db/01-crud-tasks/tasks/tasks.service.ts
@@ -1,16 +1,60 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { UpdateTaskDto } from "./dto/update-task.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Task } from "./entities/task.entity";
+import { Repository } from "typeorm";
+import { GetTasksDto } from "./dto/get-tasks.dto";
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {}
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>,
+  ) {}
 
-  async findAll() {}
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.taskRepository.create(createTaskDto)
+    return this.taskRepository.save(task)
+  }
 
-  async findOne(id: number) {}
+  async findAll(query: GetTasksDto): Promise<Task[]> {
+    const {
+      page,
+      pageSize = 1
+    } = query
 
-  async update(id: number, updateTaskDto: UpdateTaskDto) {}
+    if (page) {
+      const [tasks] = await this.taskRepository.findAndCount({
+        order: {
+          id: "ASC"
+        },
+        skip: (page - 1) * pageSize,
+        take: pageSize
+      })
+      if (!tasks.length) throw new NotFoundException("No tasks found.")
+      return tasks
+    }
 
-  async remove(id: number): Promise<void> {}
+    return this.taskRepository.find()
+  }
+
+  async findOne(id: number): Promise<Task> {
+    const task = await this.taskRepository.findOne({ where: { id } })
+    if (!task) throw new NotFoundException("Task not found")
+
+    return task
+  }
+
+  async update(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    const task = await this.findOne(id)
+    Object.assign(task, updateTaskDto)
+
+    return await this.taskRepository.save(task)
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.findOne(id) // Не совсем понимаю зачем здесь проверка на существование задачи
+    await this.taskRepository.delete(id)
+  }
 }
