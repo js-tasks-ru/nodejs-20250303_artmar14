6a885651d8695771002fb6de5bab1dfb984c8689
artm4r <artmar14@gmail.com>
2025-04-03 12:45:00 +0300

    solution for `06-nosql-db-01-mongodb

--- 06-nosql-db/01-mongodb/config/db.ts
+++ 06-nosql-db/01-mongodb/config/db.ts
@@ -1,5 +1,5 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs('database', () => ({
-  uri: process.env.MONGODB_URI
+  uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1/05-db-02-mongodb'
 }))
